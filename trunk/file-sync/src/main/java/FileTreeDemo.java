16c550ba39914b07fabb70c9bff24de35cb42168
ercos41@gmail.com <ercos41@gmail.com@180c060e-2d7f-45b1-5a2d-abc195bea189>
2012-02-21 18:43:01 +0000

    Check Tree Model Funcional

--- trunk/file-sync/src/main/java/FileTreeDemo.java
+++ trunk/file-sync/src/main/java/FileTreeDemo.java
@@ -1,122 +1,40 @@
 
+import com.jidesoft.plaf.LookAndFeelFactory;
 import com.jidesoft.swing.CheckBoxTree;
 import java.io.File;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
-import javax.swing.JTree;
-import javax.swing.event.TreeModelListener;
-import javax.swing.tree.TreeModel;
-import javax.swing.tree.TreePath;
+import javax.swing.tree.TreeSelectionModel;
 
 public class FileTreeDemo {
 
   public static void main(String[] args) {
+    LookAndFeelFactory.installDefaultLookAndFeelAndExtension();
+    JFrame.setDefaultLookAndFeelDecorated(true);
+
     // Figure out where in the filesystem to start displaying
     File root;
     if (args.length > 0) {
       root = new File(args[0]);
     } else {
-      root = new File(System.getProperty("user.home"));
+      root = new File("D:/ErikArcos/Musica");
     }
 
     // Create a TreeModel object to represent our tree of files
     FileTreeModel model = new FileTreeModel(root);
     CheckBoxTree checkBoxTree = new CheckBoxTree(model);
-    checkBoxTree.setCheckBoxEnabled(true);
-
-    // Create a JTree and tell it to display our model
-    JTree tree = new JTree();
-    tree.setModel(model);
+    checkBoxTree.getCheckBoxTreeSelectionModel().setSelectionMode(TreeSelectionModel.DISCONTIGUOUS_TREE_SELECTION);
+    checkBoxTree.getCheckBoxTreeSelectionModel().setDigIn(true);
+    checkBoxTree.setSelectPartialOnToggling(true);
 
     // The JTree can get big, so allow it to scroll
-    JScrollPane scrollpane = new JScrollPane(tree);
+    JScrollPane scrollpane = new JScrollPane(checkBoxTree);
 
     // Display it all in a window and make the window appear
     JFrame frame = new JFrame("FileTreeDemo");
     frame.getContentPane().add(scrollpane, "Center");
     frame.setSize(400, 600);
     frame.setVisible(true);
-  }
-}
-
-/**
- * The methods in this class allow the JTree component to traverse the file
- * system tree and display the files and directories.
- *
- */
-class FileTreeModel implements TreeModel {
-  // We specify the root directory when we create the model.
-
-  protected File root;
-
-  public FileTreeModel(File root) {
-    this.root = root;
-  }
-
-  // The model knows how to return the root object of the tree
-  @Override
-  public Object getRoot() {
-    return root;
-  }
-
-  // Tell JTree whether an object in the tree is a leaf
-  @Override
-  public boolean isLeaf(Object node) {
-    return ((File) node).isFile();
-  }
-
-  // Tell JTree how many children a node has
-  @Override
-  public int getChildCount(Object parent) {
-    String[] children = ((File) parent).list();
-    if (children == null) {
-      return 0;
-    }
-    return children.length;
-  }
-
-  // Fetch any numbered child of a node for the JTree.
-  // Our model returns File objects for all nodes in the tree.  The
-  // JTree displays these by calling the File.toString() method.
-  @Override
-  public Object getChild(Object parent, int index) {
-    String[] children = ((File) parent).list();
-    if ((children == null) || (index >= children.length)) {
-      return null;
-    }
-    return new File((File) parent, children[index]);
-  }
-
-  // Figure out a child's position in its parent node.
-  @Override
-  public int getIndexOfChild(Object parent, Object child) {
-    String[] children = ((File) parent).list();
-    if (children == null) {
-      return -1;
-    }
-    String childname = ((File) child).getName();
-    for (int i = 0; i < children.length; i++) {
-      if (childname.equals(children[i])) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  // This method is invoked by the JTree only for editable trees.
-  // This TreeModel does not allow editing, so we do not implement
-  // this method.  The JTree editable property is false by default.
-  @Override
-  public void valueForPathChanged(TreePath path, Object newvalue) {
-  }
-
-  // Since this is not an editable tree model, we never fire any events,
-  // so we don't actually have to keep track of interested listeners
-  @Override
-  public void addTreeModelListener(TreeModelListener l) {
-  }
-
-  @Override
-  public void removeTreeModelListener(TreeModelListener l) {
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 }
