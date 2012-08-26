6692443e39ce71cd2216c068c7a6b5620d7c0484
htaimal <htaimal@gmail.com>
2012-08-26 03:46:04 +0000

    Listado de movimientos

--- ws-android/warehouse-colfrigos/src/com/itconsultores/colfrigos/control/Movement.java
+++ ws-android/warehouse-colfrigos/src/com/itconsultores/colfrigos/control/Movement.java
@@ -4,15 +4,49 @@ import java.util.ArrayList;
 
 public class Movement {
 
-	private String type;
+	enum MovementType {
+		IN_OUT("salida-entrada"), IN("entrada"), OUT("salida");
+
+		private String movementType;
+
+		private MovementType(String c) {
+			movementType = c;
+		}
+
+		public String getMovementType() {
+			return movementType;
+		}
+	}
+	
+	private MovementType movementType;
 	private ArrayList<MovementDetail> movementDetails;
 
-	public String getType() {
-		return type;
+	
+	public Movement(String movementType, ArrayList<MovementDetail> movementDetails)throws IllegalArgumentException {
+		if (movementType == null || movementDetails == null || movementDetails.isEmpty()) {
+			throw new IllegalArgumentException();
+		}				
+		movementTypeLoop: for (MovementType mt : MovementType.values()) {
+			if (mt.getMovementType().equals(movementType)) {
+				this.movementType = mt;
+				break movementTypeLoop;
+			}
+		}
+
+		if (this.movementType == null) {
+			throw new IllegalArgumentException();
+		}
+		
+		this.movementDetails = movementDetails;
+
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public MovementType getMovementType() {
+		return movementType;
+	}
+
+	public void setMovementType(MovementType movementType) {
+		this.movementType = movementType;
 	}
 
 	public ArrayList<MovementDetail> getMovementDetails() {
