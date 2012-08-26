8a53977d467fb5fcd602c757ac812081e6873a23
htaimal <htaimal@gmail.com>
2012-08-26 03:44:57 +0000

    Listado de movimientos

--- ws-android/warehouse-colfrigos/src/com/itconsultores/colfrigos/android/FormInActivity.java
+++ ws-android/warehouse-colfrigos/src/com/itconsultores/colfrigos/android/FormInActivity.java
@@ -20,19 +20,6 @@ public class FormInActivity extends AbstractForm {
 
 	private String[] clientArray;
 
-	static final String KEY_NAME = "estado";
-
-	static final String KEY_MOVEMENTS = "movimientos";
-	static final String KEY_MOVEMENT = "movimiento";
-	static final String KEY_ID = "id";
-	static final String KEY_TYPE = "tipo";
-	static final String KEY_LABEL = "etiqueta";
-	static final String KEY_WEIGHT = "peso";
-	static final String KEY_ROLLING = "balanceo";
-
-	static final String KEY_IN = "entra";
-	static final String KEY_OUT = "sale";
-
 	public FormInActivity() {
 		super(R.layout.form_entrada);
 	}
@@ -63,39 +50,7 @@ public class FormInActivity extends AbstractForm {
 				.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 		clientSpinner.setAdapter(clientArrayAdapter);
 
-		// ArrayList<Movement> movements=new ArrayList<Movement>();
-		// NodeList nodeMovements = doc.getElementsByTagName(KEY_MOVEMENTS);
-		// for (int i = 0; i < nodeMovements.getLength(); i++) {
-		// Movement movement= new Movement();
-		// Element e = (Element)positionNodes.item(i);
-		// movement.setType(parser.getValue(e, KEY_TYPE));
-		// ArrayList<MovementDetail> movementDetails=new
-		// ArrayList<MovementDetail>();
-		// if(movement.getType()=="salida-entrada"){
-		//
-		// //TODO
-		// }else{
-		// MovementDetail movementDetail= new MovementDetail();
-		// movementDetail.setId(parser.getValue(e, KEY_ID));
-		// movementDetail.setCar(parser.getValue(e, KEY_CAR));
-		// movementDetail.setCoordinate(parser.getValue(e, KEY_COORDINATE));
-		// movementDetail.setLabel(parser.getValue(e, KEY_LABEL));
-		// movementDetail.setRolling(parser.getValue(e, KEY_ROLLING));
-		// movementDetail.setSubtype(null);//TODO
-		// movementDetail.setWeight(parser.getValue(e, KEY_WEIGHT));
-		//
-		// movementDetails.add(movementDetail);
-		// }
-		// movement.setMovementDetails(movementDetails);
-		// movements.add(movement);
-		//
-		// }
-		//
-		//
-		// Toast.makeText(this, "Car:"
-		// +myCar.getName()+" Positions:"+myCar.getPositions().size()+" Movements:"+movements.size(),
-		// Toast.LENGTH_SHORT);
-	}
+		}
 
 	@Override
 	public void onClick(View view) {
