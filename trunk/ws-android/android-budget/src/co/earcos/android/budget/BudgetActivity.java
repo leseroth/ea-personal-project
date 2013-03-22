473d61380439119e643564da85ce924b01759009
ercos41@gmail.com <ercos41@gmail.com@180c060e-2d7f-45b1-5a2d-abc195bea189>
2013-03-22 16:28:17 +0000

    Dropbox test

--- trunk/ws-android/android-budget/src/co/earcos/android/budget/BudgetActivity.java
+++ trunk/ws-android/android-budget/src/co/earcos/android/budget/BudgetActivity.java
@@ -8,29 +8,54 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class BudgetActivity extends Activity implements OnClickListener  {
+public class BudgetActivity extends Activity implements OnClickListener {
 
 	private Button testButton;
 	private TextView testText;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_budget);
-        
-        testButton = (Button) findViewById(R.id.test_button);
-        testText = (TextView) findViewById(R.id.test_text);
-        
-        testButton.setOnClickListener(this);
-    }
-    
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.screen_budget);
+
+		testButton = (Button) findViewById(R.id.test_button);
+		testText = (TextView) findViewById(R.id.test_text);
+
+		testButton.setOnClickListener(this);
+	}
+
 	@Override
 	public void onClick(View view) {
 
 		if (view.equals(testButton)) {
-			Log.i("test","button pressed");
+			Log.i("test", "button pressed");
 			testText.setText("sssssss");
-		} 
+		}
 	}
+
+	// protected void onResume() {
+	// super.onResume();
+	//
+	// // ...
+	//
+	// if (mDBApi.getSession().authenticationSuccessful()) {
+	// try {
+	// // MANDATORY call to complete auth.
+	// // Sets the access token on the session
+	// mDBApi.getSession().finishAuthentication();
+	//
+	// AccessTokenPair tokens = mDBApi.getSession()
+	// .getAccessTokenPair();
+	//
+	// // Provide your own storeKeys to persist the access token pair
+	// // A typical way to store tokens is using SharedPreferences
+	// storeKeys(tokens.key, tokens.secret);
+	// } catch (IllegalStateException e) {
+	// Log.i("DbAuthLog", "Error authenticating", e);
+	// }
+	// }
+	//
+	// // ...
+	// }
 
 }
