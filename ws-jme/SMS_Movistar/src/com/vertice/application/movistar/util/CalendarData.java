02eb8649d46e8f013ad050b4ef9db27e1e620887
ercos41 <ercos41@gmail.com>
2013-11-29 12:31:30 +0000

    Mensaje de consulta: Llave de acceso aleatoria y padding con espacios para la clave del vendedor

--- ws-jme/SMS_Movistar/src/com/vertice/application/movistar/util/CalendarData.java
+++ ws-jme/SMS_Movistar/src/com/vertice/application/movistar/util/CalendarData.java
@@ -3,28 +3,26 @@ package com.vertice.application.movistar.util;
 import java.util.Calendar;
 
 public class CalendarData {
-	
-	private static Calendar calendar = Calendar.getInstance();
-	
-	public static String getDate(){
-		String res="";		
-		res = getFixedString(calendar.get(Calendar.YEAR),4)+getFixedString(calendar.get(Calendar.MONTH)+1,2)+getFixedString(calendar.get(Calendar.DAY_OF_MONTH),2);
-		return res;
-	}
-	
-	public static String getHour(){
-		String res="";
-		res = getFixedString(calendar.get(Calendar.HOUR_OF_DAY),2)+""+getFixedString(calendar.get(Calendar.MINUTE),2)+""+getFixedString(calendar.get(Calendar.SECOND),2);
-		return res;
-	}
-	
-	private static String getFixedString(int aux,int c){
-		String res = aux+"";
-		while(res.length()<c){
-			res="0"+res;
-		}
-		return res;
-	}
-	
-	
+
+    public static String getDate() {
+        Calendar calendar = Calendar.getInstance();
+        return getFixedString(calendar.get(Calendar.YEAR), 4)
+                + getFixedString(calendar.get(Calendar.MONTH) + 1, 2)
+                + getFixedString(calendar.get(Calendar.DAY_OF_MONTH), 2);
+    }
+
+    public static String getHour() {
+        Calendar calendar = Calendar.getInstance();
+        return getFixedString(calendar.get(Calendar.HOUR_OF_DAY), 2)
+                + getFixedString(calendar.get(Calendar.MINUTE), 2)
+                + getFixedString(calendar.get(Calendar.SECOND), 2);
+    }
+
+    private static String getFixedString(int aux, int c) {
+        String res = aux + "";
+        while (res.length() < c) {
+            res = "0" + res;
+        }
+        return res;
+    }
 }
